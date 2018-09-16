73f4d092392293413c705cfb745a1d6de1fd743c
wang <530912423@qq.com>
2018-09-16 16:11:02 +0800

    update the project test

--- src/test/java/testGit.java
+++ src/test/java/testGit.java
@@ -1,19 +1,21 @@
 import VCS.GitService;
 import VCS.impl.GitServiceImpl;
 import project.Project;
-import project.ProjectBranch;
 
 public class testGit {
 
     public static void main(String args[]){
         Project project = Project.newProject();
 
+        int b = 0;
+
         GitService gitService = new GitServiceImpl();
         gitService.cloneRepo(project.getPluginConfiguration().getCloneUrl(), project.getPluginConfiguration()
                 .getLocalDirectory(), project.getPluginConfiguration().getUserName(), project.getPluginConfiguration()
                 .getPassword());
-        for (String branch : gitService.listBranches(project.getPluginConfiguration().getLocalDirectory())) {
-            project.addBranch(new ProjectBranch(project, branch));
-        }
+//        for (String branch : gitService.listBranches(project.getPluginConfiguration().getLocalDirectory())) {
+//            project.addBranch(new ProjectBranch(project, branch));
+//        }
+
     }
 }
